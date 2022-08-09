883e5432142b4cec45b36ea504e5373d9701ab0f
Nina Chuang <aa070312@au.edu.tw>
2022-08-09 17:26:40 +0800

    20220809

--- Lineindex.js
+++
@@ -1,8 +0,0 @@
-var linebot = require('linebot');
-var express = require('express');
-
-var bot = linebot({
-  channelId: 1657343574,
-  channelSecret: c8d540889324fbf6facaa9953e952642,
-  channelAccessToken: HLJxWPAi/+rOQrVSGTDLDHXchbGP+tsiatLjOKzOngceoyiN6Ig8MiKphymBKu/DStD7FImTwUC4AvSqA5EfogoBcXRT5CgQRtGButGk5b56KnCgslAq+YBmLD1IGQEeo9dizPBYbJiySZ5Ds7o7LwdB04t89/1O/w1cDnyilFU=
-});
